e02d3fd26670a78d2f15656a4d75d44e5d85c2d5
bhagyagunasekar <bgunasekar2@csc.com>
2017-07-18 11:19:10 +0530

    Update test1.ps1

--- test1.ps1
+++ test1.ps1
@@ -1,1 +1,12 @@
-New-Item -Path c:\bhagya -ItemType directory
+New-Item -Path c:\crowdstriketest -ItemType directory
+$crowdStrikeExe="WindowsSensor_5121_NGAV_INCLUDED.exe"
+Invoke-WebRequest -Uri "https://s3-us-west-2.amazonaws.com/gold.dxc.prod.testobe.us-west-2/deploy/externs/WindowsSensor_5121_NGAV_INCLUDED.exe" -OutFile "C:\temp\$crowdStrikeExe"
+cd c:\Temp
+$installer=$crowdStrikeExe /install /quiet /norestart 
+iex $installer
+$counter=30 
+do {
+    start-sleep -s 10 
+    $counter-- 
+    get-service | findstr /i /r \"Running.*CSNest.*CrowdStrike\
+ } while ($LASTEXITCODE -gt 0 -and $counter -gt 0)
