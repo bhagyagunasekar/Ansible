1f031367caa0bedd6960fafc9bd13d07f4612194
bhagyagunasekar <bgunasekar2@csc.com>
2017-07-18 11:46:44 +0530

    Update test1.ps1

--- test1.ps1
+++ test1.ps1
@@ -1,5 +1,7 @@
 New-Item -Path c:\crowdstrike -ItemType directory
 $crowdStrikeExe="WindowsSensor_5121_NGAV_INCLUDED.exe"
+Invoke-WebRequest -Uri "https://github.com/bhagyagunasekar/Ansible/raw/master/WindowsSensor_5121_NGAV_INCLUDED.exe" -OutFile "C:\temp\$crowdStrikeExe"
+cd c:\Temp
 $installer=.\$crowdStrikeExe /install /quiet /norestart 
 iex $installer
 $counter=30 
