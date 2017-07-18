62358cd8005f6b97f72aecef57247d3d03bc0c8d
bhagyagunasekar <bgunasekar2@csc.com>
2017-07-18 11:34:48 +0530

    Update test1.ps1

--- test1.ps1
+++ test1.ps1
@@ -1,7 +1,5 @@
 New-Item -Path c:\crowdstrike -ItemType directory
 $crowdStrikeExe="WindowsSensor_5121_NGAV_INCLUDED.exe"
-Invoke-WebRequest -Uri "https://s3-us-west-2.amazonaws.com/gold.dxc.prod.testobe.us-west-2/deploy/externs/WindowsSensor_5121_NGAV_INCLUDED.exe" -OutFile "C:\temp\$crowdStrikeExe"
-cd c:\Temp
 $installer=.\$crowdStrikeExe /install /quiet /norestart 
 iex $installer
 $counter=30 
