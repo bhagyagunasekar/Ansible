754c11168189c87b185c0395901f4b8322d660ed
bhagyagunasekar <bgunasekar2@csc.com>
2017-07-19 21:17:00 +0530

    Update test1.ps1

--- test1.ps1
+++ test1.ps1
@@ -2,13 +2,6 @@ New-Item -Path c:\testing -ItemType directory
 Invoke-WebRequest -Uri "https://github.com/bhagyagunasekar/Ansible/raw/master/WindowsSensor_5121_NGAV_INCLUDED.exe" -OutFile "C:\temp\WindowsSensor_5121_NGAV_INCLUDED.exe"
 cd c:\Temp
 $installer=.\WindowsSensor_5121_NGAV_INCLUDED.exe /install /quiet /norestart CID="35C43E7262224DFB9AA9F142596987E5-E7"
-iex $installer
-$counter=30 
-do {
-    $counter-- 
-    get-service | findstr /i /r "Running.*CSNest.*CrowdStrike"
- } while ($LASTEXITCODE -gt 0 -and $counter -gt 0)
-
 write-host "Installing and Enabling SSM agent"
 Invoke-WebRequest -Uri "https://s3.amazonaws.com/ec2-downloads-windows/EC2Config/EC2Install.zip" -OutFile "C:\Temp\EC2Install.zip"
 $BackUpPath = "C:\Temp\EC2Install.zip"
